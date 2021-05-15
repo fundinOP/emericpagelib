440e539dd5d7d9e1fdf5c77f691dee9277badeba
fundinOP <77344587+fundinOP@users.noreply.github.com>
2021-05-15 22:36:38 +0200

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,13 +8,13 @@ class Home extends BaseController
 	{
 
         $page = new Page('monTitre');
-        $page->addHeader('/headers/base_header.php');
-        $page->addNav('/navs/base_nav.php', [
+        $page->addHeader('/headers/base_header.html');
+        $page->addNav('/navs/base_nav.html', [
             'username' => 'Emeric'
 
         ]);
-        $page->addContainer('/containers/index_container.php');
-        $page->addFooter('/footers/base_footer.php');
+        $page->addContainer('/containers/index_container.html');
+        $page->addFooter('/footers/base_footer.html');
         $page->setLinks([['type' => 'stylesheet', 'href' => '/css/style.css'], ['type' => 'stylesheet', 'href' => '/css/index.css']]);
         $page->setScrits([['src' => '/scripts/main.js']]);
         return $page->display();
